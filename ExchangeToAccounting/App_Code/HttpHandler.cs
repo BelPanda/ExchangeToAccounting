9433ec62e3c1e59c2eb4c5595ac5d602689eb167
Aliaksandr <panda.judo@outlook.com>
2020-03-20 17:26:16 +0300

    Create solution

---
+++ ExchangeToAccounting/App_Code/HttpHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Routing;
+
+namespace DefaultNamespace
+{
+    public class MyRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new HttpHandler();
+        }
+    }
+    public class HttpHandler: IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            string result = "<p>Ваш IP: " + context.Request.UserHostAddress + "</p>";
+            result += "<p>UserAgent: " + context.Request.UserAgent + "</p>";
+            context.Response.Write(result);
+        }
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+    }
+}
